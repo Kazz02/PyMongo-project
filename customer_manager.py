2a6716aa1b5138a7eb8026c26c82af0e8866903e
Kazz02 <jesse.partonen@edu.turkuamk.fi>
2025-12-01 11:55:01 +0200

    Added add, update and delete customer

--- customer_manager.py
+++ customer_manager.py
@@ -17,9 +17,27 @@ def main():
         elif choice == '4':
          break
 
+def add_customer():
+    name = input("Enter customer name: ")
+    address = input("Enter customer address: ")
+    customer = {"name": name, "address": address}
+    mycol.insert_one(customer)
+    print("Customer added successfully.")
+
 def view_customers():
     for x in mycol.find():
         print(f"{x['name']:<10} | {x['address']}")
+
+def update_customer():
+    name = input("Enter the name of the customer to update: ")
+    new_address = input("Enter new address: ")
+    mycol.update_one({"name": name}, {"$set": {"address": new_address}})
+    print("Customer info updated.")
     
+def delete_customer():
+    name = input("Enter the name of the customer to delete: ")
+    mycol.delete_one({"name": name})
+    print("Customer deleted.")
+
 if __name__ == "__main__":
     main()
