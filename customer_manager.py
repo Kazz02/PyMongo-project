6cb8d372b6bd4403afcbad6fba92ae6d6d7c4162
Kazz02 <jesse.partonen@edu.turkuamk.fi>
2025-12-01 11:50:23 +0200

    Added customer view

--- customer_manager.py
+++ customer_manager.py
@@ -1,4 +1,8 @@
+import pymongo
 
+myclient  = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["mydatabase"]
+mycol = mydb["customers"]
 
 def main():
     while True:
@@ -7,8 +11,15 @@ def main():
         print("3. Delete Customer")
         print("4. Exit")
         choice = input("Enter choice: ")
-        if choice == '4':
+
+        if choice == '2':
+            view_customers()
+        elif choice == '4':
          break
 
+def view_customers():
+    for x in mycol.find():
+        print(f"{x['name']:<10} | {x['address']}")
+    
 if __name__ == "__main__":
     main()
