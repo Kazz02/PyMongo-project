d0f096f74c93b1de1f247377cce391f7a76c7e46
Kazz02 <jesse.partonen@edu.turkuamk.fi>
2025-12-01 15:27:27 +0200

    Updated requirements and updated the api to get all customers

--- api.py
+++ api.py
@@ -1,4 +1,10 @@
 from fastapi import FastAPI
+import pymongo 
+
+myclient  = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["mydatabase"]
+mycol = mydb["customers"]
+
 
 app = FastAPI()
 
@@ -7,3 +13,13 @@ app = FastAPI()
 async def read_root():
     return {"message": "Hello World"}
 
+@app.get("/customers/")
+async def get_customers():
+    customers = []
+    for x in mycol.find():
+        customers.append({
+            "name": x.get("name", ""),
+            "nickname": x.get("nickname", ""),
+            "address": x.get("address", "")
+        })
+    return customers
